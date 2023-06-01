79ec547066d6fed0029bf2e9eef4ed7e0b3b35f6
pgk95 <kalupgk@gmail.com>
2023-06-01 07:54:59 -0700

    quick fix on 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,15 +9,14 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+
+	size_t size = 0;
+
+	if (tree)
 	{
-		return (0);
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
 	}
-
-	size_t size = 1;
-
-	size += binary_tree_size(tree->left);
-	size += binary_tree_size(tree->right);
-
 	return (size);
 }
