166e8acc339af61574ca66c22a3c4db54499e36a
pgk95 <kalupgk@gmail.com>
2023-06-01 07:59:40 -0700

    quick fix on 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,19 +10,15 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return (0);
-	}
 
 	size_t count = 0;
 
-	if (tree->left != NULL || tree->right != NULL)
-{
-		count++;
+	if (tree)
+	{
+		count += (tree->left || tree->right) ? 1 : 0;
 		count += binary_tree_nodes(tree->left);
 		count += binary_tree_nodes(tree->right);
-}
+	}
 
 	return (count);
 }
