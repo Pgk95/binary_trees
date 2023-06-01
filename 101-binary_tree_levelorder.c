4ba9d39dc9293855441f3c7b5e5c90639e494bf0
pgk95 <kalupgk@gmail.com>
2023-06-01 08:23:10 -0700

    quick fix

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,21 +1,4 @@
 #include "binary_trees.h"
-
-/**
- * struct node_s - linked list type (single)
- * @node: const binary tree node
- * @next: points to the next node
- */
-typedef struct node_s
-{
-	const binary_tree_t *node;
-	struct node_s *next;
-} ll;
-
-ll *append(ll *head, const binary_tree_t *btnode);
-void free_list(ll *head);
-ll *get_children(ll *head, const binary_tree_t *parent);
-void levels_rec(ll *head, void (*func)(int));
-
 /**
  * binary_tree_levelorder - Goes through a binary tree
  *                          using level-order traversal.
